b80544e4efbfa1b95f85ae823d6849318d28671d
Kiran <makamkiran95@gmail.com>
2021-12-20 17:38:39 +0530

    Added : Rest api for post with jason as request body

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,10 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -23,5 +27,9 @@ public class HelloController
 	@GetMapping("/get/{name}")
 	public String helloOne(@PathVariable String name) {
 		return "Hello" +" " + name;
+	}
+	@PostMapping("/post")
+	public String hello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() +  user.getLastName();
 	}
 }
