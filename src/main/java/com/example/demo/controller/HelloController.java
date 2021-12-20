8abf50b94a13b1afa8850700b6fe58ce49eca6d3
Kiran <makamkiran95@gmail.com>
2021-12-20 17:06:32 +0530

    Added: Rest api with query parameter

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloController
 	@RequestMapping(value = {"","/","/home"})
 	public String hello() {
 		return "Hello from Bridgelabz";
+	}
+	
+	@RequestMapping(value="/query",method = RequestMethod.GET)
+	public String hello(@RequestParam (value = "name") String name) {
+		return "Hello" +" " + name;
 	}
 }
